26d20477f36e09d75ca116cb7fe8f492949da32e
Seraf Dos Santos <serafim@cyb3r.ca>
2012-01-11 08:36:08 -0500

    Utilitaries in their own file

---
+++ KitScript.safariextension/scripts/_Utils.js
@@ -0,0 +1,46 @@
+/**
+ *  KitScript - A User Script Safari Extension
+ *
+ *  _Utils.js - Javascript file containing utilitary classes (by Prototype.js)
+ *  used globally in the extension.
+ *
+ *  @author Seraf Dos Santos <webmaster@cyb3r.ca>
+ *  @copyright 2011-2012 Seraf Dos Santos - All rights reserved.
+ *  @license MIT License
+ *  @version 0.1
+ */
+
+
+
+
+
+var _Utils = Class.create({
+
+    initialize: function () {
+        
+        
+    },
+    /**
+     *  @param int verboseLevel (0=Silenced,1=Console,2=BrowserAlert)
+     */
+    setVerbosityLevel: function (verboseLevel) {
+
+        this.vl = verboseLevel;
+    },
+    log: function (msg) {
+
+        switch (this.vl) {
+
+            case 2:
+                alert(msg);
+            case 1:
+                console.log(msg);
+                break;
+            case 0:
+            default:
+                // Silence
+        }
+    }
+});
+
+_Utils.vl = 0;
