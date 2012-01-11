fdff516b1a4e3d7eb03b8f1a42b275319cce10d7
Seraf Dos Santos <serafim@cyb3r.ca>
2012-01-10 22:07:14 -0500

    Documentation & button click call

--- KitScript.safariextension/scripts/MainPanel.js
+++ KitScript.safariextension/scripts/MainPanel.js
@@ -1,9 +1,25 @@
 
+/**
+ *  KitScript - A User Script Safari Extension
+ *
+ *  MainPanel.js - Javascript file containing functions for the main panel
+ *  of the extension.
+ *
+ *  @author Seraf Dos Santos
+ *  @copyright 2011-2012 Seraf Dos Santos - All rights reserved.
+ *  @license MIT License
+ *  @version 0.1
+ */
+
+
+
+
+
 // Run on load
 jQuery(document).ready(function ($) {
     
     //
-    $("#ks-topmenu * a, ul.ks-vertmenu * a, #ks-gs-form * a").click(function (evt) {
+    $("#ks-topmenu * a, ul.ks-vertmenu * a, #ks-gs-form * a, #ks-aus-form * a").click(function (evt) {
         
         var _req = $(this).attr("href");
         
@@ -27,6 +43,11 @@ jQuery(document).ready(function ($) {
                 break;
             case '#ks-gs-btn-remove':
                 ks.mainPanel.globalSettingsForm.removeExclude();
+                break;
+            
+            // New User Script
+            case '#ks-aus-btn-add':
+                ks.mainPanel.newUserScriptForm.addUserScript();
                 break;
             default:
                 ks.mainPanel.contentManager.transitContent(_req);
